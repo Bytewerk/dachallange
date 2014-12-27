e243d924001f58986c141ea655c3e6da5a4285fa
Hubert Denkmair <xor@chaostreff.in>
2014-12-27 18:38:56 +0100

    selenium example

---
+++ selenium-example/example.py
@@ -0,0 +1,25 @@
+__author__ = 'hd'
+
+# pip install -U selenium
+# wget http://selenium.googlecode.com/files/selenium-server-standalone-2.28.0.jar
+# java -jar selenium-server-standalone-2.28.0.jar
+
+# web element api:
+# http://selenium.googlecode.com/svn/trunk/docs/api/py/webdriver_remote/selenium.webdriver.remote.webelement.html
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+import time
+
+browser = webdriver.Firefox() # Get local session of firefox
+browser.get("http://www.yahoo.com") # Load page
+assert "Yahoo!" in browser.title
+elem = browser.find_element_by_name("p") # Find the query box
+elem.send_keys("seleniumhq" + Keys.RETURN)
+time.sleep(0.2) # Let the page load, will be added to the API
+try:
+    browser.find_element_by_xpath("//a[contains(@href,'http://seleniumhq.org')]")
+except NoSuchElementException:
+    assert 0, "can't find seleniumhq"
+browser.close()
